1a0f718aa6bdf87b54853f3dfb55fc050f7abb8c
Mayukh Chakravartti <mayukh.chakravartti@gmail.com>
2021-02-15 12:06:19 -0500

    cc3

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function compute()
     t = Number(document.getElementById("years").value);
     intFor  = Number(new Date().getFullYear()) + Number(t);
 
-    si = p + (p * t * (r/100));
+    si = p + (1 + (r/100) * t);
 
     res = "If you deposit " + p + "<br>";
     res += "at an interest rate of " + r + "%.<br>"
