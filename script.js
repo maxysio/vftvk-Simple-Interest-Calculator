fd5737a02a3ca8368c00cd4b4d58ef298e5d5fe4
Mayukh Chakravartti <mayukh.chakravartti@gmail.com>
2021-02-15 18:36:18 -0500

    cc4

--- script.js
+++ script.js
@@ -24,13 +24,20 @@ function compute()
     t = Number(document.getElementById("years").value);
     intFor  = Number(new Date().getFullYear()) + Number(t);
 
+    if(p<=0){
+        alert("Please enter a positive number");
+        document.getElementById("principal").value = "";
+        document.getElementById("principal").focus();
+        return;
+    }
+
     si = p * (1 + ((r/100) * t));
     si = si.toFixed(2);
     
-    res = "If you deposit " + p + ",<br>";
-    res += "at an interest rate of " + r + "%.<br>"
-    res += "You will rceive an amount of " + si + ",<br>"
-    res += "in the year " + intFor + "<br>"
+    res = "If you deposit <mark>" + p + "</mark>,<br>";
+    res += "at an interest rate of <mark>" + r + "%.</mark><br>"
+    res += "You will rceive an amount of <mark>" + si + "</mark>,<br>"
+    res += "in the year <mark>" + intFor + "</mark><br>"
     document.getElementById("result").innerHTML= res;
 }
 
