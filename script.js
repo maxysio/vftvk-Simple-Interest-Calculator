dd9554bb2876239df2dab3c92bd0c48dc2af5428
Mayukh Chakravartti <mayukh.chakravartti@gmail.com>
2021-02-15 11:59:24 -0500

    cc1

--- script.js
+++ script.js
@@ -3,10 +3,15 @@ function compute()
     p = document.getElementById("principal").value;
     r = document.getElementById("rate").value;
     t = document.getElementById("years").value;
+    intFor  = new Date().getFullYear() + t;
 
     si = p + (p * t * (r/100));
 
-    document.getElementById("result").innerText= "Result is here amigo : " + si;
+    res = "If you deposit " + p + "<br>";
+    res += "at an interest rate of " + r + "%.<br>"
+    res += "You will rceive an amount of " + si + ",<br>"
+    res += "in the year " + intFor
+    document.getElementById("result").innerHTML= res;
 
 }
         
