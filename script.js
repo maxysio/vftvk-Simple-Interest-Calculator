d93fc6e505c155ac56e86e01950df83ca4d00050
Mayukh Chakravartti <mayukh.chakravartti@gmail.com>
2021-02-15 11:44:08 -0500

    c1

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 function compute()
 {
     p = document.getElementById("principal").value;
-    
+    r = document.getElementById("rate").value;
+    t = document.getElementById("years").value;
+
+    si = p + (p * t * (r/100));
+
+    document.getElementById("result").textContent= "SI value is: " + si;
 }
         
